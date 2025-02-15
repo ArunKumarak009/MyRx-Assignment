1db4e91c025fe54f918f8e092ea020a0b9e76f83
Rayapuram Arunkumar <arunkumarrayapuram@gmail.com>
2025-02-15 10:39:02 +0530

    solution

--- sorted_squares.py
+++ sorted_squares.py
@@ -1,9 +1,9 @@
 def sortedSquares(arr):
     n = len(arr)
-    result = [0] * n  # Output array
-    print(result)
+    result = [0] * n  
+ 
     left, right = 0, n - 1
-    index = n - 1  # Start filling from the last position
+    index = n - 1 
 
     while left <= right:
         left_val, right_val = abs(arr[left]), abs(arr[right])
@@ -15,10 +15,10 @@ def sortedSquares(arr):
             result[index] = right_val ** 2
             right -= 1
         
-        index -= 1  # Move backwards in result array
+        index -= 1  
 
     return result
 
-# Example Usage:
+
 arr = [-12, -8, -7, -5, 2, 4, 5, 11, 15]
 print(sortedSquares(arr))
